65f2f6e1e8201dd28a766846810c5b552155241f
Laura <laura.hantz@hotmail.fr>
2020-01-14 14:36:36 +0100

    ok

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
 use App\Entity\Game;
 
 class GameController extends AbstractController
